48e3597a0243a0fa5f0ff8b3490eb3f8441be78c
B <henryirons@yahoo.com>
2019-12-10 23:12:27 +0100

    tripcost: adding trips

--- tripcost/server.js
+++ tripcost/server.js
@@ -5,7 +5,7 @@ const mongo = require('mongodb').MongoClient
 // Initialize the Express app:
 const app = express()
 
-const url = 'mongodb://localhost:27017' //because 27017 is the default port, this is the mongodb server url
+const url = 'mongodb://localhost:27017' // because 27017 is the default port, this is the mongodb server url
 
 // add the stubs for the API endpoints we support:
 app.post('trip', (req, res) => {
@@ -20,15 +20,30 @@ app.get('expenses', (req, res) => {
 // use the listen() method on app to start the server
 app.listen(3000, () => console.log('Server Ready'))
 
+let db,trips,expenses
 
-let db,trips,expenses 
-
-//connecting to the database
-mongo.connect(url,(err,client)=>{if(err){
+// connecting to the database
+mongo.connect(url, (err, client) => {
+  if (err) {
     console.error(err)
     return
-}
-db = client.db('tripcost')
-trips =db.collection('trips')
-expenses=db.collection('expenses')
+  }
+  db = client.db('tripcost')
+  trips = db.collection('trips')
+  expenses = db.collection('expenses')
+})
+
+app.use(express.json())
+
+app.post('/trip', (req, res) => {
+  const name = req.body.name
+  trips.insertOne({name: name}, (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({err: err})
+      return
+    }
+    console.log(result)
+    res.status(200).json({ok: true})
+  })
 })
