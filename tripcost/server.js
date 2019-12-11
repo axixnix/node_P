78b590817ceb4686219c5fbcd2c75fcd0423ed8d
B <henryirons@yahoo.com>
2019-12-11 02:48:42 +0100

    tripcost: adding trips

--- tripcost/server.js
+++ tripcost/server.js
@@ -8,8 +8,26 @@ const app = express()
 const url = 'mongodb://localhost:27017' // because 27017 is the default port, this is the mongodb server url
 
 // add the stubs for the API endpoints we support:
-app.post('trip', (req, res) => {
+app.use(express.json())
+
+app.post('/trip', (req, res) => { /*If you get an error like “could not read property insertOne of undefined”,
+     make sure trips is successfully set in mongo.connect().
+      Add a console.log(trips) before calling insertOne() to 
+      make sure it contains the collection object.*/
+  const name = req.body.name
+  trips.insertOne({name: name}, (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({err: err})
+      return
+    }
+    console.log(result)
+    res.status(200).json({ok: true})
+  })
 })
+
+
+
 app.get('trips', (req, res) => {
 })
 app.post('expense', (req, res) => {
@@ -33,17 +51,4 @@ mongo.connect(url, (err, client) => {
   expenses = db.collection('expenses')
 })
 
-app.use(express.json())
 
-app.post('/trip', (req, res) => {
-  const name = req.body.name
-  trips.insertOne({name: name}, (err, result) => {
-    if (err) {
-      console.error(err)
-      res.status(500).json({err: err})
-      return
-    }
-    console.log(result)
-    res.status(200).json({ok: true})
-  })
-})
