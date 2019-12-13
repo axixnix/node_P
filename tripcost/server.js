4c2b421bd39e429c1b86bbd58c01576ac52288aa
B <henryirons@yahoo.com>
2019-12-13 23:19:24 +0100

    add an expense

--- tripcost/server.js
+++ tripcost/server.js
@@ -37,7 +37,25 @@ app.get("trips", (req, res) => {
     res.status(200).json({ trips: items });
   });
 });
-app.post("expense", (req, res) => {});
+app.post("expense", (req, res) => {
+  expenses.insertOne(
+    {
+      trip: req.body.trip,
+      date: req.body.date,
+      amount: req.body.amount,
+      category: req.body.category,
+      description: req.body.description
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      res.status(200).json({ ok: true });
+    }
+  );
+});
 app.get("expenses", (req, res) => {});
 
 // use the listen() method on app to start the server
